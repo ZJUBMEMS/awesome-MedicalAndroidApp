65e6b923e113bb4bf3c3f2c91db37dcc389c3ab6
VamosC <3160102243@zju.edu.cn>
2018-09-09 16:20:42 +0800

    目前最新

--- app/src/main/java/com/example/zjubme/teethmanagement/WebView.java
+++ app/src/main/java/com/example/zjubme/teethmanagement/WebView.java
@@ -1,11 +1,19 @@
 package com.example.zjubme.teethmanagement;
 
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebViewClient;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class WebView extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +23,25 @@ public class WebView extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(url);
+        hideAction();
+        setBack();
+    }
 
+    private void hideAction(){
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.hide();
+        }
+    }
+
+    private void setBack(){
+        ImageButton back = (ImageButton)findViewById(R.id.webview_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(0);
+            }
+        });
     }
 }
