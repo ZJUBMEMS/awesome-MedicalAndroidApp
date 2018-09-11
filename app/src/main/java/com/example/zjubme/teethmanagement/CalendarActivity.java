970bdc4e716e2beee1d24c9b0f6e21f50b80c2c3
extw <33283557+extw@users.noreply.github.com>
2018-09-11 10:50:27 +0800

    完成直方图

--- app/src/main/java/com/example/zjubme/teethmanagement/CalendarActivity.java
+++ app/src/main/java/com/example/zjubme/teethmanagement/CalendarActivity.java
@@ -1,5 +1,6 @@
 package com.example.zjubme.teethmanagement;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,14 @@ import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
 import com.avos.avoscloud.FindCallback;
 import com.avos.avoscloud.SaveCallback;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
@@ -29,6 +38,77 @@ public class CalendarActivity extends AppCompatActivity {
     private List<CalendarViewDate> calendarViewDateList = new ArrayList<>();
     private List<CalendarDay> calendarDayList = new ArrayList<>();
     private SignDayDecorator signDayDecorator = new SignDayDecorator(CalendarActivity.this);
+    private float timeData;
+    private List<Long> timeDataList = new ArrayList<>();
+    private List<BarEntry> entries = new ArrayList<>();
+
+    public void loadtime() {
+        AVQuery<AVObject> avQueryTime = new AVQuery<>("TimeData");
+        Date NowTime = new Date();
+        long Weekdata = NowTime.getTime()/1000/60/60/24/7;
+        avQueryTime.whereEqualTo("WeekData", Weekdata);
+        avQueryTime.orderByAscending("createdAt");
+        avQueryTime.findInBackground(new FindCallback<AVObject>() {
+            @Override
+            public void done(List<AVObject> list, AVException e) {
+                BarChart chart = (BarChart) findViewById(R.id.chart);
+                if (list==null){
+                    entries.add(new BarEntry(0,0f));
+                    entries.add(new BarEntry(1,0f));
+                    entries.add(new BarEntry(2,0f));
+                    entries.add(new BarEntry(3,0f));
+                    entries.add(new BarEntry(4,0f));
+                    entries.add(new BarEntry(5,0f));
+                    entries.add(new BarEntry(6,0f));
+                    return;
+                }
+                if (e==null){
+                    for (int k=0; k<list.size(); k++){
+                        timeData = (float)(list.get(k).getLong("DaTime")/60.0);
+                        entries.add(new BarEntry(k,timeData));
+                    }
+                    for (int k=6; k>list.size()-1; k--){
+                        entries.add(new BarEntry(k,0f));
+                    }
+                }
+                BarDataSet dataSet = new BarDataSet(entries,"");
+                dataSet.setColor(Color.GREEN);//0x-(00--ff)-xxxxxx
+
+                BarData data = new BarData(dataSet);
+                data.setBarWidth(0.9f);
+                data.setValueTextColor(Color.BLACK);
+                data.setValueTextSize(12f);
+
+                chart.setData(data);
+                chart.setFitBars(true);
+                chart.invalidate();
+                chart.setScaleEnabled(false);
+                chart.setDrawGridBackground(false);
+                chart.setBorderColor(Color.BLUE);
+                chart.setDrawBorders(true);
+
+                Description good = new Description();
+                good.setText("");
+                chart.setDescription(good);
+
+                XAxis xAixs = chart.getXAxis();
+                xAixs.setDrawGridLines(false);
+
+                YAxis rightAxis = chart.getAxisRight();
+                rightAxis.setEnabled(false);
+
+                YAxis leftAxis = chart.getAxisLeft();
+                leftAxis.setAxisMaximum(22f);
+                leftAxis.setDrawGridLines(true);
+                LimitLine ll = new LimitLine(140f, "ganleni");
+                ll.setLineColor(Color.RED);
+                ll.setLineWidth(4f);
+                ll.setTextColor(Color.BLACK);
+                ll.setTextSize(12f);
+                leftAxis.addLimitLine(ll);
+            }
+        });
+    }
 
     public void save(CalendarDay day, int i){
         AVObject calendarDay = new AVObject("CalendarDay");
@@ -82,6 +162,8 @@ public class CalendarActivity extends AppCompatActivity {
         setContentView(R.layout.sign_calendar_layout);
         materialCalendarView = (MaterialCalendarView)findViewById(R.id.calendar_view);
         ActionBar actionBar = getSupportActionBar();
+
+        loadtime();
         if (actionBar != null){
             actionBar.hide();
         }
